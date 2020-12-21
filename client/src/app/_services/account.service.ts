e953c0dd615f3a20221f23458b92450cb5afdccc
jetbomber <rwarwaruk#shaw.ca>
2020-12-21 16:29:01 -0700

    End of Section 11

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -21,8 +21,7 @@ export class AccountService {
       map((response: User) => {
         const user = response;
         if (user) {
-          localStorage.setItem('user', JSON.stringify(user));
-          this.currentUserSource.next(user);
+          this.setCurrrentUser(user);
         }
       })
     )
@@ -32,8 +31,7 @@ export class AccountService {
     return this.http.post(this.baseUrl + 'account/register', model).pipe(
       map((user: User) => {
         if (user) {
-          localStorage.setItem('user',JSON.stringify(user));
-          this.currentUserSource.next(user);
+          this.setCurrrentUser(user);
         }
         return user;
       })
@@ -41,6 +39,7 @@ export class AccountService {
   }
 
   setCurrrentUser(user: User) {
+    localStorage.setItem('user',JSON.stringify(user));
     this.currentUserSource.next(user);
   }
 
