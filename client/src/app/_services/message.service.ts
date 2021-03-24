9c1c337da255fe1641b6ff65ade98ee5df80fbd8
Reagan Warwaruk <rwarwaruk@optimaxdrilling.com>
2021-03-24 09:44:36 -0600

    Completed Messaging Functionality

--- client/src/app/_services/message.service.ts
+++ client/src/app/_services/message.service.ts
@@ -12,8 +12,7 @@ export class MessageService {
 
   constructor(private http: HttpClient) { }
 
-  getMessages(pageNumber, pageSize, container)
-  {
+  getMessages(pageNumber, pageSize, container){
     let params = getPaginationHeaders(pageNumber, pageSize);
     params = params.append("Container", container);
     return getPaginatedResult<Message[]>(this.baseUrl + 'messages', params, this.http);
@@ -21,5 +20,13 @@ export class MessageService {
 
   getMessageThread(username: string) {
     return this.http.get<Message[]>(this.baseUrl + 'messages/thread/' + username);
+  }
+
+  sendMessage(username: string, content: string){
+      return this.http.post<Message>(this.baseUrl + 'messages', {recipientUsername: username, content})
+  }
+
+  deleteMessage(id: number){
+    return this.http.delete(this.baseUrl + 'messages/' + id);
   }
 }
