17cf83a02ee329bd77d0d7a15bc1bb95b8a50e07
Reagan Warwaruk <rwarwaruk@optimaxdrilling.com>
2021-03-31 10:18:49 -0600

    Refactored to use AspNet Identity

--- API/Data/Seed.cs
+++ API/Data/Seed.cs
@@ -1,32 +1,26 @@
 using System.Collections.Generic;
-using System.Security.Cryptography;
-using System.Text;
 using System.Threading.Tasks;
 using API.Entities;
 using Microsoft.EntityFrameworkCore;
 using System.Text.Json;
+using Microsoft.AspNetCore.Identity;
 
 namespace API.Data
 {
     public class Seed
     {
-        public static async Task SeedUsers(DataContext context) {
-            if (await context.Users.AnyAsync()) return;
+        public static async Task SeedUsers(UserManager<AppUser> userManager) {
+            if (await userManager.Users.AnyAsync()) return;
 
             var userData = await System.IO.File.ReadAllTextAsync("Data/UserSeedData.json");
             var users = JsonSerializer.Deserialize<List<AppUser>>(userData);
             
             foreach (var user in users){
-                using var hmac = new HMACSHA512();
                 user.UserName = user.UserName.ToLower();
-                user.PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes("Pa$$w0rd"));
-                user.PasswordSalt = hmac.Key;
 
-                context.Users.Add(user);
+                await userManager.CreateAsync(user, "Pa$$w0rd");
                 
             }
-            
-            await context.SaveChangesAsync();
 
         }
     }
